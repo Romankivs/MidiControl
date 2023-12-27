1a1b79deff139d86489cb9fdfb55046ec175400c
Sviatoslav Romankiv <romankiv1771@gmail.com>
2023-12-27 21:19:58 +0200

    Initial Commit

---
+++ MidiControl/MidiControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidiControlApp.swift
+//  MidiControl
+//
+//  Created by Sviatoslav Romankiv on 23.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct MidiControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
