491c1e1457f15d65196822afec5699e6bfd0ba08
Sviatoslav Romankiv <romankiv1771@gmail.com>
2024-01-13 23:23:57 +0200

    Add and delete animations and better selection after deletion

--- MidiControl/Views/MidiListView.swift
+++ MidiControl/Views/MidiListView.swift
@@ -47,19 +47,21 @@ struct MidiList: View {
             VStack(alignment: .leading) {
                 HStack {
                     Button(action: {
-                        let element = MidiToStroke(midi: MidiNoteOnMessage(channel: 5, note: 22, velocity: 126), stroke: [])
-                        strokes.append(element)
-                        let el = MidiToStroke(midi: MidiNoteOffMessage(channel: 5, note: 22, velocity: 126), stroke: [])
-                        strokes.append(el)
+                        withAnimation {
+                            let element = MidiToStroke(midi: MidiNoteOnMessage(channel: 5, note: 22, velocity: 126), stroke: [])
+                            strokes.append(element)
+                        }
                     }) {
                         Image(systemName: "plus")
                     }
                     Button(action: {
-                        if let selected = selectedStroke,
-                           let index = strokes.firstIndex(of: selected) {
-                            strokes.remove(at: index)
-                            if !strokes.isEmpty {
-                                selectedStroke = strokes[0]
+                        withAnimation {
+                            if let selected = selectedStroke,
+                               let index = strokes.firstIndex(of: selected) {
+                                strokes.remove(at: index)
+                                if strokes.count >= index, !strokes.isEmpty {
+                                    selectedStroke = strokes[max(0, index - 1)]
+                                }
                             }
                         }
                     }) {
@@ -81,17 +83,23 @@ struct MidiList: View {
             VStack(alignment: .leading) {
                 HStack {
                     Button(action: {
-                        let stroke = KeyStroke(keyCode: 11, keyDown: false)
-                        selectedStroke?.stroke.append(stroke)
+                        withAnimation {
+                            let stroke = KeyStroke(keyCode: 11, keyDown: false)
+                            selectedStroke?.stroke.append(stroke)
+                        }
                     }) {
                         Image(systemName: "plus")
                     }
                     Button(action: {
-                        guard let selected = selectedKey else { return }
-                        guard let index = selectedStroke?.stroke.firstIndex(of: selected) else { return }
-                        selectedStroke?.stroke.remove(at: index)
-                        if let selectedStroke = selectedStroke, !selectedStroke.stroke.isEmpty {
-                            selectedKey = selectedStroke.stroke[0]
+                        withAnimation {
+                            guard let selected = selectedKey else { return }
+                            guard let index = selectedStroke?.stroke.firstIndex(of: selected) else { return }
+                            selectedStroke?.stroke.remove(at: index)
+                            if let selectedStroke = selectedStroke,
+                               selectedStroke.stroke.count >= index,
+                               !selectedStroke.stroke.isEmpty {
+                                selectedKey = selectedStroke.stroke[max(0, index - 1)]
+                            }
                         }
                     }) {
                         Image(systemName: "minus")
